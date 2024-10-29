40e10fc690931a9d41bded3aa96b2719f98bc00c
Jared Galicia <jared0192galicia@gmail.com>
2024-10-29 12:57:15 -0600

    add _document

---
+++ src/app/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>{/* Aquí puedes añadir links a iconos, fuentes, etc. */}</Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
